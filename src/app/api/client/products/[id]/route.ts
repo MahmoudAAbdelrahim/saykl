eec46e4c9dd03384d19df3ab14b290be9a279ce0
MahmoudAAbdelrahim <mahmoudalimahmoud536@gmail.com>
2025-09-20 02:05:01 +0300

    route.ts

--- src/app/api/client/products/[id]/route.ts
+++ src/app/api/client/products/[id]/route.ts
@@ -1,21 +1,20 @@
-// src/app/api/client/products/[id]/route.ts
 import { NextRequest, NextResponse } from "next/server";
 import { connectDB } from "../../../../../lib/db";
 import Product, { IProduct } from "../../../../../backend/models/Product";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  context: { params: { id: string } }
 ) {
   try {
     await connectDB();
-    const ownerId = params.id;
+    const { id } = context.params;
 
-    if (!ownerId) {
+    if (!id) {
       return NextResponse.json({ error: "Missing ownerId" }, { status: 400 });
     }
 
-    const products: IProduct[] = await Product.find({ ownerId }).sort({
+    const products: IProduct[] = await Product.find({ ownerId: id }).sort({
       createdAt: -1,
     });
 
